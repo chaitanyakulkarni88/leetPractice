d72b80435540c676074111c22a399eb683a41fc6
Chaitanya <chaitanyakulkarni2216@gmail.com>
2026-02-07 21:30:46 +0000

    leet codes stack and linked list

---
+++ src/main/java/GameOfLife.java
@@ -0,0 +1,62 @@
+class GameOfLife {
+    public void gameOfLife(int[][] board) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int liveNeighbors = countLiveNeighbors(board, row, col, rows, cols);
+
+                if (board[row][col] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[row][col] = 2;
+                }
+
+                if (board[row][col] == 0 && liveNeighbors == 3) {
+                    board[row][col] = -1;
+                }
+            }
+        }
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (board[row][col] == 2) {
+                    board[row][col] = 0;
+                } else if (board[row][col] == -1) {
+                    board[row][col] = 1;
+                }
+            }
+        }
+    }
+
+    private int countLiveNeighbors(int[][] board, int row, int col, int rows, int cols) {
+        int liveCount = 0;
+
+        for (int neighborRow = row - 1; neighborRow <= row + 1; neighborRow++) {
+            for (int neighborCol = col - 1; neighborCol <= col + 1; neighborCol++) {
+                if (neighborRow < 0 || neighborRow >= rows ||
+                        neighborCol < 0 || neighborCol >= cols ||
+                        (neighborRow == row && neighborCol == col)) {
+                    continue;
+                }
+
+                if (board[neighborRow][neighborCol] > 0) {
+                    liveCount++;
+                }
+            }
+        }
+
+        return liveCount;
+    }
+
+    public static void main(String[] args) {
+        GameOfLife gof = new GameOfLife();
+        int[][] board = {
+                {0, 1, 0},
+                {0, 0, 1},
+                {1, 1, 1},
+                {0, 0, 0}
+        };
+        gof.gameOfLife(board);
+    }
+
+}
---
+++ src/main/java/leet/EvaluateReversePolishNotation.java
@@ -0,0 +1,42 @@
+package leet;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (String token : tokens) {
+            if (token.length() > 1 || Character.isDigit(token.charAt(0))) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int secondOperand = stack.pop();
+                int firstOperand = stack.pop();
+
+                switch (token) {
+                    case "+":
+                        stack.push(firstOperand + secondOperand);
+                        break;
+                    case "-":
+                        stack.push(firstOperand - secondOperand);
+                        break;
+                    case "*":
+                        stack.push(firstOperand * secondOperand);
+                        break;
+                    case "/":
+                        stack.push(firstOperand / secondOperand);
+                        break;
+                }
+            }
+        }
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        EvaluateReversePolishNotation erpn = new EvaluateReversePolishNotation();
+        String[] tokens = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        int result = erpn.evalRPN(tokens);
+    }
+}
